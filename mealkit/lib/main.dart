6b25db92dea82f57c49edb83f396886fdd599010
AAreesha <97911372+AAreesha@users.noreply.github.com>
2024-03-25 10:25:45 +0500

    restore original

--- mealkit/lib/main.dart
+++ mealkit/lib/main.dart
@@ -18,9 +18,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green
       ),
-      // home: HomeView(),
+      home: HomeView(),
       // home: Registration(),
-         home: MenuPage()
+        //  home: MenuPage()
     );
   }
 }
