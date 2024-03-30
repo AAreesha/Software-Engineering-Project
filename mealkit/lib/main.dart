0a978a09cb5d6d631e9d1623a9f6d65cab395758
LaibaZehra <lz07603@st.habib.edu.pk>
2024-03-30 20:34:33 +0500

    Update main.dart

--- mealkit/lib/main.dart
+++ mealkit/lib/main.dart
@@ -1,35 +1,47 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mealkit/screens/services/auth.dart';
+import 'package:provider/provider.dart';
 import 'home.dart';
 import 'registration.dart';
 import 'menupage.dart'; 
 import 'loginpage.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:mealkit/screens/authenticate/sign_in.dart';
+import 'package:provider/provider.dart';
+import 'package:mealkit/models/user.dart';
+import 'package:mealkit/screens/wrapper.dart';
 
 
-void main() {
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  sqfliteFfiInit();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyAwIaft9z3H84LD2T_gjHJTR-nxU_67U-U", 
+      appId: "1:401710519943:web:6fb9a9e8194470b24bca22", 
+      messagingSenderId: "401710519943", 
+      projectId: "mealkit-project")
+  );
   // Initialize databaseFactory before using sqflite functions
-  databaseFactory = databaseFactoryFfi;
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}); // Fix the constructor definition
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green
+    return StreamProvider<User_Details?>.value(
+      initialData: null, // Provide an initial value or null if you don't have any
+      value: AuthService().user,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: Wrapper(),
       ),
-      home: HomeView(),
-      //home: Registration()
-      // home: MenuPage(),
-      // home: Login()
     );
   }
 }
-
