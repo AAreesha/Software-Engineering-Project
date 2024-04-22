a2c3700283fa701aef1ea9656c6a27f321a95289
AAreesha <97911372+AAreesha@users.noreply.github.com>
2024-04-22 11:43:30 +0500

    babygirl

--- mealkit/lib/menupage.dart
+++ mealkit/lib/menupage.dart
@@ -24,18 +24,18 @@ class MenuPage extends StatelessWidget {
           Padding(
             padding: EdgeInsets.all(kPadding),
             child: _buildCuisineSection(context, 'Italian', [
-              _buildRecipeCard(context, 'Carbonara Pasta',
+              _buildRecipeButton(context, 'Carbonara Pasta',
                   'Creamy Italian pasta dish with pancetta, and black pepper.', 'assets/bestseller1.png'),
-              _buildRecipeCard(context, 'Margherita Pizza',
+              _buildRecipeButton(context, 'Margherita Pizza',
                   'Traditional pizza with tomatoes, mozzarella, and basil.', 'assets/bestseller1.png'),
             ]),
           ),
           Padding(
             padding: EdgeInsets.all(kPadding),
             child: _buildCuisineSection(context, 'Mexican', [
-              _buildRecipeCard(context, 'Tacos al Pastor',
+              _buildRecipeButton(context, 'Tacos al Pastor',
                   'Tacos filled with spit-grilled pork.', 'assets/bestseller1.png'),
-              _buildRecipeCard(context, 'Guacamole',
+              _buildRecipeButton(context, 'Guacamole',
                   'Classic avocado-based dip.', 'assets/bestseller1.png'),
             ]),
           ),
@@ -87,146 +87,77 @@ class MenuPage extends StatelessWidget {
     );
   }
 
-Widget _buildRecipeCard(
-    BuildContext context, String title, String description, String imagePath) {
-  return GestureDetector(
-    onTap: () {
-      // Navigate to recipe detail page when tapped
-      // Navigator.push(
-      //   context,
-      //   MaterialPageRoute(
-      //       builder: (context) => RecipeDetailPage(title, description)),
-      // );
-    },
-    child: Container(
-      width: 200, // Set the width of the card here
-      child: Card(
-        margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-        color: kSecondaryColor, // Set the background color here
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset(
-              imagePath,
-              height: 200,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
+  Widget _buildRecipeButton(
+      BuildContext context, String title, String description, String imagePath) {
+    return ElevatedButton(
+      onPressed: () {
+        // Show a dialog when the button is pressed
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text(title),
+              content: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                  Text(
-                    title,
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 8),
                   Text(
                     description,
                     style: TextStyle(fontSize: 14),
                   ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Implement Add to Cart functionality here
+                      print('$title added to cart');
+                      Navigator.of(context).pop(); // Close the dialog
+                    },
+                    child: Text('Add to Cart'),
+                  ),
                 ],
               ),
-            ),
-          ],
+            );
+          },
+        );
+      },
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.zero, // No padding
+      ),
+      child: Container(
+        width: 200, // Set the width of the card here
+        child: Card(
+          margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+          color: kSecondaryColor, // Set the background color here
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                imagePath,
+                height: 200,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      description,
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
-
-}
-
-
-
-
-// class MenuPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.green,
-//         automaticallyImplyLeading: false,
-//         title: Navigationbar(),
-//       ),
-//       body: ListView(
-//         children: [
-
-// padding: EdgeInsets.all(kPadding),
-          // _buildCuisineSection(context, 'Italian', [
-          //   _buildRecipeCard(context, 'Spaghetti Carbonara',
-          //       'A classic Italian pasta dish.'),
-          //   _buildRecipeCard(context, 'Margherita Pizza',
-          //       'Traditional pizza with tomatoes, mozzarella, and basil.'),
-          // ]),
-          // _buildCuisineSection(context, 'Mexican', [
-          //   _buildRecipeCard(context, 'Tacos al Pastor',
-          //       'Tacos filled with spit-grilled pork.'),
-          //   _buildRecipeCard(
-          //       context, 'Guacamole', 'Classic avocado-based dip.'),
-  //         ]),
-  //         // Add more cuisine sections as needed
-  //       ],
-  //     ),
-  //   );
-  // }
-
-//   Widget _buildCuisineSection(
-//       BuildContext context, String title, List<Widget> recipes) {
-//     return Column(
-//       crossAxisAlignment: CrossAxisAlignment.start,
-//       children: [
-//         Padding(
-//           padding: const EdgeInsets.all(8.0),
-//           child: Text(
-//             title,
-//             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-//           ),
-//         ),
-//         SizedBox(height: 8),
-//         SingleChildScrollView(
-//           scrollDirection: Axis.horizontal,
-//           child: Row(
-//             children: recipes,
-//           ),
-//         ),
-//         SizedBox(height: 16),
-//       ],
-//     );
-//   }
-
-//   Widget _buildRecipeCard(
-//       BuildContext context, String title, String description) {
-//     return GestureDetector(
-//       onTap: () {
-//         // Navigate to recipe detail page when tapped
-//         Navigator.push(
-//           context,
-//           MaterialPageRoute(
-//               builder: (context) => RecipeDetailPage(title, description)),
-//         );
-//       },
-//       child: Card(
-//         margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-//         child: Padding(
-//           padding: const EdgeInsets.all(16.0),
-//           child: Column(
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             children: [
-//               Text(
-//                 title,
-//                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-//               ),
-//               SizedBox(height: 8),
-//               Text(
-//                 description,
-//                 style: TextStyle(fontSize: 14),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
