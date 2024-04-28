2f9869367b5e9d686c39d6168b337ee8522fcf25
LaibaZehra <lz07603@st.habib.edu.pk>
2024-04-28 14:20:57 +0500

    Update auth.dart

--- mealkit/lib/screens/services/auth.dart
+++ mealkit/lib/screens/services/auth.dart
@@ -1,10 +1,11 @@
 import 'package:mealkit/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mealkit/screens/services/database.dart';
 
 class AuthService {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
-
+  //late UserCredential result;
   // create user obj based on firebase user
   User_Details? _userFromFirebaseUser(User? user) {
     return user != null ? User_Details(uid: user.uid) : null;
@@ -40,15 +41,36 @@ class AuthService {
 
 
   // register with email and password
-  Future registerWithEmailAndPassword(String email, String password) async {
+  Future registerWithEmailAndPassword(String email, String password, String username, String address,String contact) async {
     try {
       UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
       User? user = result.user;
+      await DatabaseService(uid: user!.uid).updateUserData(username,address,contact);
       return _userFromFirebaseUser(user);
     } catch (error) {
       print(error.toString());
       return null;
     } 
+  }
+
+  User? getCurrentUser() {
+    return _auth.currentUser;
+  }
+
+
+  // Function to add order details to Firestore
+  Future<void> addOrderDetails(String itemName, double price) async {
+    User? user = _auth.currentUser;
+
+    if (user == null) {
+      throw Exception('User is not authenticated');
+    }
+
+    try {
+      await DatabaseService(uid: user.uid).addOrder(user.uid,itemName, price);
+    } catch (e) {
+      print('Error adding order details: $e');
+    }
   }
 
   // sign out
